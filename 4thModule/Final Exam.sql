845a8d47ffa6b4fbb0eccce7f8b8855350526baf
Rafael <rafacardosodeandrade@gmail.com>
2020-06-16 13:51:02 -0500

    EXERCISE 5

--- 4thModule/Final Exam.sql
+++ 4thModule/Final Exam.sql
@@ -403,4 +403,43 @@ SELECT
   AVG(review_count)
   FROM user
 
-  
+--5
+
+SELECT  
+  city,
+  SUM (review_count) AS Number_of_reviews
+  FROM business
+  GROUP BY city
+  ORDER BY Number_of_reviews DESC
+
+  --OUTPUT
++-----------------+-------------------+
+| city            | Number_of_reviews |
++-----------------+-------------------+
+| Las Vegas       |             82854 |
+| Phoenix         |             34503 |
+| Toronto         |             24113 |
+| Scottsdale      |             20614 |
+| Charlotte       |             12523 |
+| Henderson       |             10871 |
+| Tempe           |             10504 |
+| Pittsburgh      |              9798 |
+| Montréal        |              9448 |
+| Chandler        |              8112 |
+| Mesa            |              6875 |
+| Gilbert         |              6380 |
+| Cleveland       |              5593 |
+| Madison         |              5265 |
+| Glendale        |              4406 |
+| Mississauga     |              3814 |
+| Edinburgh       |              2792 |
+| Peoria          |              2624 |
+| North Las Vegas |              2438 |
+| Markham         |              2352 |
+| Champaign       |              2029 |
+| Stuttgart       |              1849 |
+| Surprise        |              1520 |
+| Lakewood        |              1465 |
+| Goodyear        |              1155 |
++-----------------+-------------------+
+(Output limit exceeded, 25 of 362 total rows shown)  

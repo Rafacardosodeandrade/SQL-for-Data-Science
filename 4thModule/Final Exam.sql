4fa0369c3e630af9a92f0eb52f59332089816fbe
Rafael <rafacardosodeandrade@gmail.com>
2020-06-16 14:33:07 -0500

    Exercisse 10

--- 4thModule/Final Exam.sql
+++ 4thModule/Final Exam.sql
@@ -557,4 +557,27 @@ WHERE text LIKE '%hate%'
 |      232 |
 +----------+
 
-10.
+-- 10.Find the top 10 users with the most fans:
+--	SQL code used to arrive at answer:
+
+SELECT 
+  name,
+  fans
+FROM user
+ORDER BY fans DESC
+LIMIT 10	
+	
++-----------+------+
+| name      | fans |
++-----------+------+
+| Amy       |  503 |
+| Mimi      |  497 |
+| Harald    |  311 |
+| Gerald    |  253 |
+| Christine |  173 |
+| Lisa      |  159 |
+| Cat       |  133 |
+| William   |  126 |
+| Fran      |  124 |
+| Lissa     |  120 |
++-----------+------+
